b9f1d17750d221a5e12c433797ce361811340777
tatialveso <tatiialveso@gmail.com>
2021-11-05 14:31:25 -0300

    :sparkles: create post get method

--- src/main/java/org/generation/blogPessoal/controller/PostController.java
+++ src/main/java/org/generation/blogPessoal/controller/PostController.java
@@ -1,8 +1,13 @@
 package org.generation.blogPessoal.controller;
 
+import java.util.List;
+
+import org.generation.blogPessoal.model.Post;
 import org.generation.blogPessoal.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 	@Autowired
 	private PostRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Post>> GetAll() {
+		return ResponseEntity.ok(repository.findAll());
+	}
 }
