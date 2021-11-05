bfabd1e872fc47724c6cb6e5b09cf2a0f074e38f
tatialveso <tatiialveso@gmail.com>
2021-11-05 14:59:53 -0300

    :sparkles: create find by id post method

--- src/main/java/org/generation/blogPessoal/controller/PostController.java
+++ src/main/java/org/generation/blogPessoal/controller/PostController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +23,12 @@ public class PostController {
 	public ResponseEntity<List<Post>> GetAll() {
 		return ResponseEntity.ok(repository.findAll());
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Post> GetById(@PathVariable long id) {
+		return repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
 }
