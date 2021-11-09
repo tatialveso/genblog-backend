cf88cb5f2688cb075f96ee77a2697af5cb075b66
tatialveso <tatiialveso@gmail.com>
2021-11-09 09:00:16 -0300

    :sparkles: create post post method

--- src/main/java/org/generation/blogPessoal/controller/PostController.java
+++ src/main/java/org/generation/blogPessoal/controller/PostController.java
@@ -5,10 +5,13 @@ import java.util.List;
 import org.generation.blogPessoal.model.Post;
 import org.generation.blogPessoal.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +37,11 @@ public class PostController {
 	@GetMapping("/title/{title}")
 	public ResponseEntity<List<Post>> GetByTitle(@PathVariable String title) {
 		return ResponseEntity.ok(repository.findAllByTitleContainingIgnoreCase(title));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Post> post (@RequestBody Post post) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(post));
 	}
 	
 }
